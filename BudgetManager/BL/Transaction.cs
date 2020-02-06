56eea995a19804c78484339e1ae3630eba0b1613
Igor Zeler <igorzeler@yahoo.co.uk>
2020-02-06 19:43:10 +0000

    add Transaction class to the project

---
+++ BudgetManager/BL/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManager.BL
+{
+    abstract class Transaction
+    {
+        public int Id;
+        public string Name;
+        public decimal Amount;
+        public DateTime Date;
+    }
+}
