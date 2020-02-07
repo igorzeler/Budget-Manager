0d1c4c40d6ceb3a958e32e4867a60b10fba4de24
Igor Zeler <igorzeler@yahoo.co.uk>
2020-02-07 21:10:44 +0000

    implementation IDatabase interface in the File class

--- BudgetManager/Models/DB/File.cs
+++ BudgetManager/Models/DB/File.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BudgetManager.Models.DB;
+using Models.BudgetManager.BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace Models.BudgetManager.DB
 {
-    class File
+    class File : IDatabase
     {
+        Transaction IDatabase.Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDatabase.Write(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
