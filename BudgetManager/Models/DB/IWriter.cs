f3016b39f2242d79971c71077d2545b647e1192b
Igor Zeler <igorzeler@yahoo.co.uk>
2020-02-08 23:42:38 +0000

    add Write method to IWiter interface

--- BudgetManager/Models/DB/IWriter.cs
+++ BudgetManager/Models/DB/IWriter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BudgetManager.Models.BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace BudgetManager.Models.DB
 {
     interface IWriter
     {
+        void Write(Transaction transaction);
     }
 }
