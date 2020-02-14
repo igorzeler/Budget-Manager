1ccfdf795c270cce1e4d07410f80eb3f10c646a7
Igor Zeler <igorzeler@yahoo.co.uk>
2020-02-14 21:26:10 +0000

    add switch-case statement to RunSelected method in the Program class

--- BudgetManager/Program.cs
+++ BudgetManager/Program.cs
@@ -35,7 +35,26 @@ namespace BudgetManager
         }
         private static void RunSelected(string selected)
         {
+            switch (selected)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+                default:
+                    break;
 
+            }
         }
     }
 }
