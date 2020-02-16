8c4c4108b3b29be23638ce014e96b31a0d9aed14
Igor Zeler <igorzeler@yahoo.co.uk>
2020-02-16 20:39:50 +0000

    add contructor to the Service class

--- BudgetManager/Models/Service.cs
+++ BudgetManager/Models/Service.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BudgetManager.Models.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,11 @@ namespace BudgetManager.Models
 {
     class Service
     {
+        public Service(IReader reader, IWriter writer)
+        {
+
+        }
+
         public void AddIncome(decimal amount, string name, DateTime date)
         {
 
