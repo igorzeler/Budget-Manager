dd86e46426877e1bbaa8c478286555500300dc3d
Igor Zeler <igorzeler@yahoo.co.uk>
2020-02-19 18:47:03 +0000

    AddOutcome() Content Update

--- BudgetManager/Program.cs
+++ BudgetManager/Program.cs
@@ -72,6 +72,22 @@ namespace BudgetManager
         private static void AddOutcome()
         {
             Console.Clear();
+
+            Console.WriteLine("New outcome");
+
+            Console.WriteLine("Name: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Amount: ");
+            var value = Console.ReadLine();
+            var amount = decimal.Parse(value);
+
+            Console.WriteLine("Date: ");
+            value = Console.ReadLine();
+            DateTime date = DateTime.Parse(value);
+
+            Service service = new Service(_file, _file);
+            service.AddOutcome(amount, name, date);
         }
         private static void AddIncome()
         {
