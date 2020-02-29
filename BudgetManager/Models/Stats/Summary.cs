bb615a52c191f4502d0cb700365c7c1819269977
Igor Zeler <igorzeler@yahoo.co.uk>
2020-02-29 22:39:41 +0000

    add field _reader

--- BudgetManager/Models/Stats/Summary.cs
+++ BudgetManager/Models/Stats/Summary.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BudgetManager.Models.DB;
 
 namespace BudgetManager.Models.Stats
 {
     class Summary
     {
+        private IReader _reader;
     }
 }
