ba892a570deb4e27261fc1db9dafcbeb66540e21
Igor Zeler <igorzeler@yahoo.co.uk>
2020-03-02 20:17:58 +0000

    implement IWriter in the Writer class

--- BudgetManager/Models/DB/Writer.cs
+++ BudgetManager/Models/DB/Writer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BudgetManager.Models.BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace BudgetManager.Models.DB
 {
-    class Writer
+    class Writer : IWriter
     {
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
