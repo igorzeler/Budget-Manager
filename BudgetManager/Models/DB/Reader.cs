a1703eecb9bdd47cdb5012ab87d626fca15045aa
Igor Zeler <igorzeler@yahoo.co.uk>
2020-03-02 20:16:43 +0000

    implement IReader in the Reader class

--- BudgetManager/Models/DB/Reader.cs
+++ BudgetManager/Models/DB/Reader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BudgetManager.Models.BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace BudgetManager.Models.DB
 {
-    class Reader
+    class Reader : IReader
     {
+        public int GetNextId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Transaction> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
