56c224390eb0f449348ed105453e49ded988ee6c
Igor Zeler <igorzeler@yahoo.co.uk>
2020-03-04 12:08:47 +0000

    add class MenuReport

--- BudgetManager/Program.cs
+++ BudgetManager/Program.cs
@@ -17,48 +17,11 @@ namespace BudgetManager
             _reader = new Reader("db.txt");
             _writer = new Writer("db.txt");
             var menu = new Menu();
-            var selected = "";
-            do
-            {
-                DisplayMenu();
-                selected = Console.ReadLine();
-                RunSelected(selected);
-            }
-            while (selected != "6");
-        }
-        private static void DisplayMenu()
-        {
-            Console.Clear();
-            Console.WriteLine("1 - Transaction list");
-            Console.WriteLine("2 - Monthly summary");
-            Console.WriteLine("3 - Add outcome");
-            Console.WriteLine("4 - Add income");
-            Console.WriteLine("5 - Remove transaction");
-            Console.WriteLine("6 - Exit");
-            Console.WriteLine("Chosen action: ");
-        }
-        private static void RunSelected(string selected)
-        {
-            switch (selected)
-            {
-                case "1":
-                    ShowList();
-                    break;
-                case "2":
-                    ShowReport();
-                    break;
-                case "3":
-                    AddOutcome();
-                    break;
-                case "4":
-                    AddIncome();
-                    break;
-                case "5":
-                    RemoveTransaction();
-                    break;
-            }
-        }
 
+            menu.RunMenu();
+            
+        }
+        
         private static void ShowList()
         {
             Console.Clear();
