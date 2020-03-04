78e1d4b0a867e79d6ecd1fbb6f0f42c2cef160f3
Igor Zeler <igorzeler@yahoo.co.uk>
2020-03-04 13:10:20 +0000

    move AddIncome() from Program to MenuIncome class

--- BudgetManager/Models/BL/MenuContent/MenuIncome.cs
+++ BudgetManager/Models/BL/MenuContent/MenuIncome.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BudgetManager.Models.DB;
+using BudgetManager.Models.Services;
 
 namespace BudgetManager.Models.BL.MenuContent
 {
     class MenuIncome
     {
+        private static IWriter _writer;
+        private static IReader _reader;
+        public static void AddIncome()
+        {
+            Console.Clear();
+
+            Console.WriteLine("New income");
+
+            Console.WriteLine("Name: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Amount: ");
+            var value = Console.ReadLine();
+            var amount = decimal.Parse(value);
+
+            Console.WriteLine("Date: ");
+            value = Console.ReadLine();
+            DateTime date = DateTime.Parse(value);
+
+            Service service = new Service(_reader, _writer);
+            service.AddIncome(amount, name, date);
+        }
     }
 }
