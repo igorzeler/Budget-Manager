99dfb1cae1e35b5252643bf65f665f981309a4e3
Igor Zeler <igorzeler@yahoo.co.uk>
2020-03-04 14:16:59 +0000

    add DbAccess class

---
+++ BudgetManager/Models/DB/DBAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManager.Models.DB
+{
+    internal static class DbAccess
+    {
+        public static IWriter SetDbWriter()
+        {
+            IWriter writer = new Writer("db.txt");
+            return writer;
+        }
+        public static IReader SetDbReader()
+        {
+            IReader reader = new Reader("db.txt");
+            return reader;
+        }
+    }
+}
