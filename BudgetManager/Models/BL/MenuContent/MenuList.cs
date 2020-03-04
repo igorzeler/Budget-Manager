d2e0bc674092ebefad1da8ae336e1a52ec57676e
Igor Zeler <igorzeler@yahoo.co.uk>
2020-03-04 12:33:57 +0000

    add menuContent classes to track

---
+++ BudgetManager/Models/BL/MenuContent/MenuList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BudgetManager.Models.DB;
+using BudgetManager.Models.Stats;
+
+namespace BudgetManager.Models.BL.MenuContent
+{
+    class MenuList
+    {
+        private static IReader _reader;
+        private static void ShowList()
+        {
+            Console.Clear();
+            Console.WriteLine("Display transactions");
+
+            List list = new List(_reader);
+
+            list.DisplayList();
+            Console.ReadKey();
+        }
+    }
+}
